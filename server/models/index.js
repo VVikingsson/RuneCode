942bd5921443a3b72dc108a75222af0a218ae7dc
Ville Vikingsson <villevi@student.chalmers.se>
2025-11-14 03:07:49 +0100

    #2 create user schema

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,10 @@
 // This file acts as a single point of import for other files wanting to access the MongoDB models
 
 const Example = require("./Example.js");
+const User = require("./User.js");
 
 module.exports = {
-    Example //,
+    Example,
+    User //,
     // other Models goes here
 }
