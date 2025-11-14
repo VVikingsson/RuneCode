ccff8b3f2400e3a9aed64ae5573002c684aee85f
Ville Vikingsson <villevi@student.chalmers.se>
2025-11-14 03:15:00 +0100

    #2 add user endpoints

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 
-const express = require("express");
-const exampleRouter = require("./exampleRoutes")
+const express = require('express');
+const exampleRouter = require('./exampleRoutes');
+const userRouter = require('./userRoutes');
 
 const router = express.Router();
 
 // Any endpoint we want to access from exampleRouter has to be prefixed with /example.
-router.use("/example", exampleRouter)
+router.use('/example', exampleRouter);
+router.use('/user', userRouter);
 
 module.exports = router;
