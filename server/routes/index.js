4c9e221d73b453a73dd33bd00878b7c1f55ac3ab
Waheeba Rasul <waheeba@chalmers.se>
2025-11-17 17:40:34 +0100

    #20 add test case routes to the routes index

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const exampleRouter = require('./exampleRoutes');
 const userRouter = require('./userRoutes');
+const testCaseRouter = require('./testCaseRoutes');
 
 const router = express.Router();
 
 // Any endpoint we want to access from exampleRouter has to be prefixed with /example.
 router.use('/example', exampleRouter);
 router.use('/users', userRouter);
+router.use('/testCases', testCaseRouter);
 
 module.exports = router;
