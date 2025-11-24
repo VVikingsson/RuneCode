bcaceaed98d371b629f4b89311c5b84224be1288
Ulyana Dranitsa <dranitsa@student.chalmers.se>
2025-11-24 16:56:36 +0300

    #11 add title field and timestamps to Submission schema

--- server/models/Submission.js
+++ server/models/Submission.js
@@ -3,10 +3,12 @@ const {Schema} = require("mongoose");
 
 const SubmissionSchema = new mongoose.Schema({
     code: {type: String, required: true},
+    title: {type: String, default: ''},
     authorNote: {type: String},
-    authorId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
-    challengeId: {type: Schema.Types.ObjectId, ref: 'Challenge', required: true},
-});
+    author: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    challenge: {type: Schema.Types.ObjectId, ref: 'Challenge', required: true},
+
+}, {timestamps: true});
 
 
 module.exports = mongoose.model('Submission', SubmissionSchema);
