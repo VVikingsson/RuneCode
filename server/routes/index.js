a8bf3cee1ddd5b40e3a4b07d651f659b727ebff7
Ulyana Dranitsa <dranitsa@student.chalmers.se>
2025-11-24 17:17:49 +0300

    modify configuration and dependency files

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@ const exampleRouter = require('./exampleRoutes');
 const userRouter = require('./userRoutes');
 const testCaseRouter = require('./testCaseRoutes');
 const challengeRouter = require('./challengeRoutes');
+const submissionRouter = require('./submissionRoutes');
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use('/example', exampleRouter);
 router.use('/users', userRouter);
 router.use('/testCases', testCaseRouter);
 router.use('/challenges', challengeRouter);
+router.use('/submissions', submissionRouter);
 
 module.exports = router;
