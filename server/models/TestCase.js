43c330ac97a496be95f8251e281f10222d9a502a
Ville Vikingsson <villevi@student.chalmers.se>
2025-12-02 16:17:28 +0100

    #25 add db seeder

--- server/models/TestCase.js
+++ server/models/TestCase.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 const TestCaseSchema = new mongoose.Schema({
 
     input: {type: String, required: true},
-    expectedOutput: {type: String, required: true},
+    expectedOutput: {type: String, required: isRequired}, // allow empty string
     language: {type: String, required: true},
 
 });
 
+function isRequired () {
+    return typeof this.expectedOutput === 'string' ? false : true
+}
 
 module.exports = mongoose.model('TestCase', TestCaseSchema);
