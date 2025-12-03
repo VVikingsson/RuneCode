8a59ca9a62fdb1395730bfb06f3489df28d5a3e6
Waheeba Rasul <waheeba@chalmers.se>
2025-12-03 01:00:34 +0100

    #27 add challenges view to router

--- client/src/router.js
+++ client/src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
+import Challenges from './views/Challenges.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: Home }
+  { path: '/', name: 'home', component: Home },
+  { path: '/', name: 'challenges', component: Challenges}
 ]
 
 const router = createRouter({
---
+++ client/src/views/Challenges.vue
@@ -0,0 +1,37 @@
+<template>
+    <BContainer class="main-container">
+        <BRow class="top-row">
+        <BCol class="left-col">
+            <h1 id="welcome-message">Start a battle of your choosing. Beware of the difficulty ahead!</h1>
+        </BCol>
+        <BCol>
+        </BCol>
+        </BRow>
+    </BContainer>
+</template>
+
+<script>
+export default {
+    name: 'challenges'
+}
+</script>
+
+<style scoped>
+    .main-container * {
+        font-family: 'Kedebideri'
+    }
+    .main-container {
+        min-width: 100% !important;
+        /* border: 2px, solid, var(--light-blue); */
+    }
+    .top-row {
+        padding-top: 5rem;
+    }
+    .left-col {
+        margin-left: 5rem;
+    }
+    #welcome-message {
+        text-align: left;
+        font-size: 3.5rem;
+    }
+</style>
