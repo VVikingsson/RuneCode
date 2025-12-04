2fecbcae6e35d8054e6d6bde0708fc2717b47cbc
Ulyana Dranitsa <dranitsa@student.chalmers.se>
2025-12-05 02:36:10 +0300

    #3 modify leaderboard to make every user a clickable link

--- client/src/components/Leaderboard.vue
+++ client/src/components/Leaderboard.vue
@@ -1,7 +1,11 @@
 <script setup>
 import { Api } from '@/Api'
 import { ref, onMounted } from 'vue'
-// select certain attributes from the response that we receive(for leaderboard we need username and points)
+// this is the attributes that will be displayed
+const fields = [
+  { key: 'username', label: 'User' },
+  { key: 'points', label: 'Points' }
+]
 const items = ref([])
 function getTopUsers() {
   Api.get('/leaderboard')
@@ -11,6 +15,7 @@ function getTopUsers() {
       // parsed attributes
       items.value = users.map((user) => {
         return {
+          id: user._id, // it is included in the items but is not displayed in the table
           username: user.username,
           points: user.points
         }
@@ -36,8 +41,18 @@ onMounted(() => {
       <BTable
         hover
         :items="items"
+        :fields="fields"
         class="my-table text-start"
-      />
+      >
+        <template #cell(username)="scope">
+          <RouterLink
+            :to="{ name: 'User', params: { id: scope.item.id } }"
+            class="text-decoration-none username-link"
+          >
+            {{ scope.item.username }}
+          </RouterLink>
+        </template>
+      </BTable>
     </BCard>
   </div>
 </template>
@@ -49,6 +64,13 @@ onMounted(() => {
   box-sizing: border-box;
   margin: auto;
   padding: 0.7em;
+}
+.username-link {
+  color: white;
+}
+.username-link:hover {
+  color: hotpink;
+  opacity: 0.8;
 }
 .my-table :deep(td),
 .my-table :deep(th) {
