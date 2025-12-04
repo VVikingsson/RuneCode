13ab8ac867a98ea0e8725c21d6f64e980383aa18
Ulyana Dranitsa <dranitsa@student.chalmers.se>
2025-12-04 18:32:14 +0300

    #17 modify config files

--- client/src/router.js
+++ client/src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
+import Leaderboard from '@/components/Leaderboard.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: Home }
+  { path: '/', name: 'home', component: Home },
+  { path: '/leaderboard', component: Leaderboard }
 ]
 
 const router = createRouter({
