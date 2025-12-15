20cfa7ab2dc490262abe8cb5c5503e4319193276
Ville Vikingsson <villevi@student.chalmers.se>
2025-12-15 16:56:35 +0100

    #7 integrate codemirror for better code editor

--- client/src/router.js
+++ client/src/router.js
@@ -5,8 +5,10 @@ import Leaderboard from '@/components/Leaderboard.vue'
 import Challenge from './views/Challenge.vue'
 import User from './views/User.vue'
 import Challenges from './views/Challenges.vue'
+import test from './views/Test.vue';
 
 const routes = [
+  { path: '/test', name: 'test', component: test },
   { path: '/', name: 'home', component: Home },
   { path: '/leaderboard', component: Leaderboard },
   { path: '/challenges', name: 'challenges', component: Challenges},
@@ -14,6 +16,7 @@ const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/challenges/:id', name: 'Challenge', component: Challenge },
   { path: '/users/:id', name: 'User', component: User, props: true } // redirects you to a page of a specific user
+
 ]
 
 const router = createRouter({
