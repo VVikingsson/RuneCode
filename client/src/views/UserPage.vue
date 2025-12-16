ebc0d217f288050d00adedb1c1d218712c9fbb13
Ulyana Dranitsa <dranitsa@student.chalmers.se>
2025-12-16 19:12:04 +0100

    #30 add submissions container with table with dummy data

--- client/src/views/UserPage.vue
+++ client/src/views/UserPage.vue
@@ -1,14 +1,27 @@
 <script setup>
+import {BCard, BCardImg, BCol, BContainer, BImg, BRow, BTab, BTabs} from "bootstrap-vue-next";
+import {ref} from "vue";
 
-import {BCard, BCardImg, BCol, BContainer, BImg, BRow} from "bootstrap-vue-next";
+//submissions table logic
+const submissionsFields = [
+  { key: 'submissionTitle', label: 'Your Solution' },
+  { key: 'challengeName', label: 'Challenge' }
+]
+const userSubmissions = [
+  { submissionTitle: 'two girls', challengeName: 'Challenge 12' },
+  { submissionTitle: 'one mom', challengeName: 'Challenge 34' },
+  { submissionTitle: 'i love AI', challengeName: 'Challenge 45' },
+  { submissionTitle: '4 times in a row', challengeName: 'Challenge 45' },
+  { submissionTitle: 'my grandpa taught me', challengeName: 'Challenge 56' }
+]
 </script>
 
 <template>
   <div class="div1">
-    <BContainer fluid class="bv-example-row p-0 mt-3">
+    <BContainer fluid class="main-con p-2 mt-3 d-flex flex-column gap-5">
 
       <BContainer
-        class="flex-wrap d-flex align-items-stretch rounded-3 gap-3"
+        class="profile-header flex-wrap d-flex align-items-stretch rounded-3 gap-3 p-0"
         fluid
       >
         <BCol class="profile-col p-3 rounded-4">
@@ -34,24 +47,43 @@ import {BCard, BCardImg, BCol, BContainer, BImg, BRow} from "bootstrap-vue-next"
           <BRow class="h-100">
             <BCol class="chall-no-col d-flex flex-column justify-content-center">
               <div>17</div>
-              <div>challenges solved</div>
+              <div>Challenges Mastered</div>
             </BCol>
             <BCol class="diff-col p-4 gap-4">
               <div>
-                <div>easy</div>
+                <div>Easy</div>
                 <div>10</div>
               </div>
               <div>
-                <div>medium</div>
+                <div>Medium</div>
                 <div>5</div>
               </div>
               <div>
-                <div>hard</div>
+                <div>Hard</div>
                 <div>2</div>
               </div>
             </BCol>
           </BRow>
         </BCol>
+      </BContainer>
+<!-- submissions container -->
+      <BContainer class="table-con p-4 rounded-4">
+        <BTabs content-class="" class="my-tabs">
+          <BTab
+            title="Solutions"
+            active
+            class="first-tab"
+          >
+            <BTable
+              v-if="userSubmissions.length > 0"
+              hover
+              :items="userSubmissions"
+              :fields="submissionsFields"
+              class="submissions-table text-start"
+            >
+            </BTable>
+          </BTab>
+        </BTabs>
       </BContainer>
     </BContainer>
   </div>
@@ -59,8 +91,46 @@ import {BCard, BCardImg, BCol, BContainer, BImg, BRow} from "bootstrap-vue-next"
 </template>
 
 <style scoped>
+.table-con {
+  background-color: var(--card-bg);
+  border: 1px solid var(--primary-blue);
+}
+.first-tab {
+  background-color: var(--card-bg);
+}
 
-.bv-example-row {
+.my-tabs :deep(.nav-tabs) {
+  background-color: transparent;
+  border-bottom: none;
+}
+.submissions-table :deep(th) {
+  background-color: transparent;
+  color: white;
+  border: none;
+  padding: 15px;
+}
+.submissions-table {
+  border-collapse: separate;
+  border-spacing: 0 15px;
+}
+.submissions-table :deep(tbody tr td:first-child) {
+  border-top-left-radius: 16px;
+  border-bottom-left-radius: 16px;
+  border-right: none;
+}
+
+.submissions-table :deep(tbody tr td:last-child) {
+  border-top-right-radius: 16px;
+  border-bottom-right-radius: 16px;
+  border-left: none;
+}
+.submissions-table :deep(td){
+  color: white;
+  background-color: rgba(74, 120, 255, 0.5);
+  padding: 15px;
+  border: 1px solid var(--primary-blue);
+}
+.main-con {
   width: 90%;
 }
 .profile-col {
